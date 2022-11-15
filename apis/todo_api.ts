2556bd09f889336608cf9df5451fef3853ce092d
Adrien Gouacide <adrien.gouacide@gmail.com>
2022-11-15 16:46:06 +0100

    commit

--- apis/todo_api.ts
+++ apis/todo_api.ts
@@ -8,10 +8,10 @@ router.post("/", async (req, res, next)=>{
     try{
         const {description} = req.body
         const createdTodo = await pool.query(
-            "INSERT INTO todo (description) VALUES($1)",
+            "INSERT INTO todo (description) VALUES($1) RETURNING *",
             [description]
         )
-        res.json(createdTodo).status(200)
+        res.json(createdTodo.rows[0]).status(200)
     }catch (err){
         console.error(err)
         res.json(err).status(400)
@@ -48,11 +48,11 @@ router.put("/:id", async (req, res, next)=>{
         const {description} = req.body
 
         const updatedTodo = await pool.query(
-            "UPDATE todo SET description = $1 WHERE todo_id = $2",
+            "UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *",
             [description, id]
         )
 
-        res.json(updatedTodo).status(200)
+        res.json(updatedTodo.rows[0]).status(200)
     }catch (err){
         console.error(err)
         res.json(err).status(400)
